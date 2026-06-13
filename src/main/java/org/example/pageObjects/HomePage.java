407220f101585fee50c6691d081fe670ba4d8549
akshatdab <akshatgupta926@gmail.com>
2026-06-13 20:27:13 +0530

    added testlisterner

--- src/main/java/org/example/pageObjects/HomePage.java
+++ src/main/java/org/example/pageObjects/HomePage.java
@@ -11,6 +11,9 @@ public class HomePage extends PageBase {
         this.driver= driver;
     }
     String lastItem="//div[text()=\"Test.allTheThings() T-Shirt (Red)\"]";
+    String backButton ="//button[@class='btn btn_secondary back btn_large inventory_details_back_button']";
+    String dropdown="//select[@class='product_sort_container']";
+    String homePageHeader="//button[@class='btn btn_primary btn_small btn_inventory ']";
 
 
     public void clickLastItem(){
@@ -18,4 +21,33 @@ public class HomePage extends PageBase {
             clickElement(lastItem, "Last item is present", 10);
         }
     }
+
+    public void clickBack(){
+        clickElement(backButton, "navigate back", 10);
+    }
+
+    public void rightclickLastItemAndOpenInNewTab(){
+        contextClick(lastItem, "Last item right click", 10);
+        robotKeyAction(lastItem, "Last item right click", 10);
+
+    }
+
+    public void validateHomeHeader(){
+        if (ElementPresent(homePageHeader,"Validate Home header is visible", 10)){
+            System.out.println("Element is visible");
+        }
+    }
+
+
+    public void selectDropdown() throws InterruptedException {
+        selectDropdown(dropdown, 0, 10);
+        Thread.sleep(2000);
+        selectDropdown(dropdown, 1, 10);
+        Thread.sleep(2000);
+        selectDropdown(dropdown, 2, 10);
+        Thread.sleep(2000);
+        selectDropdown(dropdown, 3, 10);
+
+    }
+
 }
--- src/test/java/org/example/tests/SanitySuite.java
+++ src/test/java/org/example/tests/SanitySuite.java
@@ -1,5 +1,6 @@
 package org.example.tests;
 
+import org.example.Listeners.RetryAnalyzer;
 import org.example.base.TestBase;
 import org.example.pageObjects.HomePage;
 import org.example.pageObjects.LoginPage;
@@ -11,29 +12,54 @@ import org.testng.annotations.Test;
 public class SanitySuite extends TestBase {
 
 
-    @Test(priority = 0, description ="To get the title of the website", enabled = true)
-    public void Tc_01_LaunchingBrowswer(){
+    @Test(retryAnalyzer = RetryAnalyzer.class, priority = 0, description = "To get the title of the website", enabled = true)
+    public void Tc_01_LaunchingBrowswer() {
         String title = driver.getTitle();
         System.out.println(title);
     }
 
-@Test(priority = 1, description= "To login to the website")
-public void login() throws InterruptedException {
-    LoginPage loginPage = new LoginPage(driver);
-    HomePage homePage= new HomePage(driver);
-    loginPage.enterUserName();
-    loginPage.enterPassword();
-    loginPage.clickLoginButton1();
-    Thread.sleep(5000);
-loginPage.scrollByCordinates();
+    @Test(retryAnalyzer = RetryAnalyzer.class, priority = 1, description = "To login to the website")
+    public void login() throws InterruptedException {
+        LoginPage loginPage = new LoginPage(driver);
+        HomePage homePage = new HomePage(driver);
+        loginPage.enterUserName();
+        loginPage.enterPassword();
+        loginPage.clickLoginButton1();
+        Thread.sleep(5000);
+homePage.validateHomeHeader();
+        loginPage.scrollByCordinates();
+    }
+
+    @Test(priority = 2, description = "To click last item", enabled = true)
+    public void clickLastItemInHomePage() {
+        HomePage homePage = new HomePage(driver);
+        homePage.clickLastItem();
+    }
+
+    @Test(priority = 3, description = "To right click and open the item in new tab")
+    public void clickLastItemInHomePageFromNewTab() throws InterruptedException {
+        HomePage homePage = new HomePage(driver);
+        LoginPage loginPage = new LoginPage(driver);
+        homePage.clickBack();
+        loginPage.scrollByCordinates();
+        Thread.sleep(3000);
+        homePage.rightclickLastItemAndOpenInNewTab();
+        homePage.switchMultipleWindow("child");
+        clickLastItemInHomePage();
+        homePage.switchMultipleWindow("parent");
+        Thread.sleep(5000);
+    }
+
+    @Test(priority = 4, description = "To select the dropdown using select class")
+    public void selectDropdowns() throws InterruptedException {
+        HomePage homePage = new HomePage(driver);
+        LoginPage loginPage = new LoginPage(driver);
+        homePage.scrollToTop();
+        Thread.sleep(2000);
+        homePage.selectDropdown();
+        Thread.sleep(5000);
+    }
 }
 
-@Test(priority = 2, description="To click last item")
-    public void clickLastItemInHomePage(){
-    HomePage homePage= new HomePage(driver);
-    homePage.clickLastItem();
-}
-
-}
 
 
